3c07e23356cc8410b1da782510cbfabaacae1fb6
JecaTatu <gas5@cin.ufpe.br>
2021-06-16 21:51:46 -0300

    create base for test with terratest

---
+++ test/upload_s3_file_test.go
@@ -0,0 +1,37 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestS3(t *testing.T) {
+	testFolder := test_structure.CopyTerraformFolderToTemp(t, "../", "main")
+
+	awsRegion := aws.GetRandomRegion(t, nil, nil)
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: testFolder,
+
+		Vars: map[string]interface{}{
+			"region": awsRegion,
+		},
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	bucket_name := terraform.Output(t, terraformOptions, "bucket_name")
+	file1_name := terraform.Output(t, terraformOptions, "file1_name")
+
+	file1_content := aws.GetS3ObjectContents(t, awsRegion, bucket_name, file1_name)
+
+	aws.AssertS3BucketExists(t, awsRegion, bucket_name)
+	assert.NotNil(t, file1_content)
+
+}
